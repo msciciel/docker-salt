5b754acbd5d825792233eda316ca3b1fbe482c85
msciciel <msciciel@users.noreply.github.com>
2016-07-17 11:20:37 +0200

    Clean up

--- prompt.sh
+++ prompt.sh
@@ -3,10 +3,3 @@ if [ $UID -eq 0 ]; then
 else
         PS1='\[\033[0;32m\]\u\[\033[0;32m\]@\[\033[1;32m\]\H\[\033[0;33m\] \w \[\033[1m\]\$\[\033[0m\] '
 fi
-
-alias ll="ls -la --color=always"
-alias less="less -r"
-
-export HISTFILESIZE=100000
-export HISTSIZE=100000
-export HISTTIMEFORMAT="%F %H:%M:%S "
